ebc1661d5b190ee56bf66f10e97661ee50826f71
Joao Cordeiro <jlcordeiro@gmail.com>
2022-02-05 23:42:09 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const AnchorEarnPackage = require('@anchor-protocol/anchor-earn'); //import Anchor Earn
+const AnchorEarn = AnchorEarnPackage.AnchorEarn;
+const CHAINS = AnchorEarnPackage.CHAINS;
+const DENOMS = AnchorEarnPackage.DENOMS;
+const NETWORKS = AnchorEarnPackage.NETWORKS;
+
+
+const getAnchorInfo = async (terra_address, res, next) => {
+    try {
+        const anchorEarn = new AnchorEarn({
+            chain: CHAINS.TERRA,
+            network: NETWORKS.COLUMBUS_5,
+            address: terra_address
+        });
+
+        const userBalance = await anchorEarn.balance({
+            currencies: [DENOMS.UST],
+        });
+
+        const marketInfo = await anchorEarn.market({
+            currencies: [
+                DENOMS.UST
+            ],
+        });
+
+        return {
+            Current_Balance: userBalance.balances[0].deposit_balance,
+            APY: marketInfo.markets[0].APY * 100,
+            Timestamp: marketInfo.timestamp
+        };
+    }
+    catch (exception) {
+        console.error("Exception thrown", exception.stack);
+        return { "message": "Error Occured!" };
+    }
+
+};
+
+(async () => {
+    console.log(await getAnchorInfo("???"))
+})()
